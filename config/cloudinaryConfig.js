1bdbd9af00f4926f844e4baa2489f0226f034e4c
karimibrahimramadan <karimibrahimramadan@gmail.com>
2022-12-22 14:52:51 +0200

    Bug fixes

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,7 +1,7 @@
 const cloudinary = require("cloudinary");
 
 cloudinary.config({
-  cloud_name: "djxkmfdnh",
-  api_key: "155916962535262",
-  api_secret: "GnhgnvKBqHYaAFhu262m_WuBFfc",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
